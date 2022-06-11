258dd96b877a17789381162d9830f128db20cbc5
Valeria Proshachenko <vproshachenko@gmail.com>
2022-06-11 18:21:04 +0300

    fix styles

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -4,6 +4,7 @@ import envelope from '../../images/envelope.svg';
 import styles from './button.module.css';
 
 function Button({ gmail }) {
+
   return (
     <button className={styles.btn} type="button">
       <img src={envelope} alt="" />
